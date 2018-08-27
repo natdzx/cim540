f487a204700322029b6e06892f6902f0c6667b31
Natalia Diaz <nid5@miami.edu>
2018-08-27 13:43:25 -0400

    first copy

---
+++ week1/frog.js
@@ -0,0 +1,16 @@
+function setup() {
+  // put setup code here
+  createCanvas (800, 400);
+  background (204);
+}
+
+function draw() {
+  //head
+  ellipse (150,100,150,70);
+  //mouth
+  ellipse (150,110,70,15);
+  //left eye
+  ellipse (110,65,20,20);
+  //right eye
+  ellipse (185,65,20,20);
+}
---
+++ week2/frogcolor.js
@@ -0,0 +1,19 @@
+function setup() {
+  // put setup code here
+  createCanvas (800, 400);
+  background (204);
+}
+
+function draw() {
+  //head
+  fill(255, 71, 135);
+  ellipse (150,100,150,70);
+  //mouth
+  fill(255, 71, 135);
+  ellipse (150,110,70,15);
+  //left eye
+  fill(255, 71, 135);
+  ellipse (110,65,20,20);
+  //right eye
+  ellipse (185,65,20,20);
+}
