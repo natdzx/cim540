13a6d9ab78fe33263af2735821cfe1c486142d87
Natalia Diaz <nid5@miami.edu>
2018-09-24 14:37:18 -0400

    responsive r/g squares

---
+++ week5/responses.js
@@ -0,0 +1,28 @@
+function setup(){
+  createCanvas(480,120);
+  background(204);
+}
+
+function draw(){
+  if (mouseIsPressed) {
+    if(mouseX>90 && mouseX<150 && mouseY>30 && mouseY<90){
+      fill(255,0,0);
+    } else {
+      fill(255);
+    }
+    rect(90,30,60,60);
+
+    if(mouseX>330 && mouseX<420 && mouseY>30 && mouseY<90){
+      fill(0,255,0);
+    }else {
+      fill(255);
+    }
+    rect(330,30,60,60);
+
+  }else {
+    fill(255);
+    rect(90,30,60,60);
+    rect(330,30,60,60);
+  }
+
+}
